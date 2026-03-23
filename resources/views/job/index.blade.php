382354d619f999d5b228504a639cf9cbb0b929e2
SandakalumSilva <sandakalum1996@gmail.com>
2026-03-23 13:15:06 +0530

    input fields to add close button

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -1,11 +1,12 @@
 <x-layout>
     <x-breadcrumbs class="mb-2" :links="['Jobs' => route('jobs.index')]" />
     <x-card class="mb-4 text-sm">
-        <form action="{{ route('jobs.index') }}" method="GET">
+        <form id="filtering-form" action="{{ route('jobs.index') }}" method="GET">
             <div class="mb-4 grid grid-cols-2 gap-4">
                 <div>
                     <div class="mb-1 font-semibold">Search</div>
-                    <x-text-input name="search" value="{{ request('search') }}" placeholder="Search for any text" />
+                    <x-text-input name="search" value="{{ request('search') }}" placeholder="Search for any text"
+                        form-id="filtering-form" />
                 </div>
                 <div>
                     <div class="mb-1 font-semibold">Salary</div>
