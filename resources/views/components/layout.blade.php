dbe2950401ff582ceba5823c66d7f31f100f8848
SandakalumSilva <sandakalum1996@gmail.com>
2026-03-24 21:45:15 +0530

    install alpine js and button component added

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
 
 <body class="mx-auto mt-10 max-w-2xl bg-slate-200 text-slate-700">
